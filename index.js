558c1280688f66dd50cd3b0be46b256992fe92cc
dovid yarmush <yarmushda@gmail.com>
2023-05-14 14:56:05 -0400

    completed filter lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(driverArr, string) {
+    return driverArr.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+}
+
+const fuzzyMatch = function(arr, str) {
+    return arr.filter(driver => {
+       if (driver.slice(0, 2) === str) {
+        return driver;
+       }
+    })
+}
+
+const matchName = (drivers, name) => {
+  return drivers.filter(driver => {
+    return driver.name === name;
+  })
+}
